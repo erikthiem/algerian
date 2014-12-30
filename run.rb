091e6a9ff33d340f5e9fb80b9b18d5cbadb8884f
Erik Thiem <erikthiem@gmail.com>
2014-12-29 21:18:03 -0500

    added about us page

--- run.rb
+++ run.rb
@@ -7,3 +7,23 @@ end
 get '/article/:article_number' do
     erb :article
 end
+
+get '/pages/contactus' do
+    erb :contactus
+end
+
+get '/pages/aboutus' do
+    erb :aboutus
+end
+
+get '/pages/archives' do
+    erb :archives
+end
+
+get '/pages/authors' do
+    erb :authors
+end
+
+get '/pages/staff' do
+    erb :staff
+end
